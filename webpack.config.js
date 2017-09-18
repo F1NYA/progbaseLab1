befebebea18637b21a171b61d12555d57155d60c
Dmitry Pintak <poloabusesuckaz@gmail.com>
2017-09-18 20:59:58 +0300

    dynamcy added files

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,26 @@ var extractPlugin = new ExtractTextPlugin({
     filename: 'index.css'
 });
 
-module.exports = {
+/* FUNCTION TO GET PUGS */
+var getPugsFromFolder = function (dir) {
+    var filesystem = require("fs");
+    var results = [];
+    filesystem.readdirSync(dir).forEach(function (file) {
+        file = dir + '/' + file;
+        var stat = filesystem.statSync(file);
+        if (stat && stat.isDirectory()) {
+            results = results.concat(_getAllFilesFromFolder(file))
+        } else results.push(file);
+    });
+    let filenames = [];
+    results.map((item)=>{
+        let reg = /[^/]*.pug/.exec(item)
+        if(reg) filenames.push(reg[0].substring(0,reg[0].length - 4));
+    });
+    return filenames;
+};
+
+let config = {
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -22,8 +41,8 @@ module.exports = {
                     path.resolve(__dirname, 'node_modules/'),
                 ]
             },
-            { 
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
                 loader: 'url-loader',
                 options: {
                     limit: 10000,
@@ -32,9 +51,9 @@ module.exports = {
                     name: '[name].[ext]'
                 }
             },
-            { 
-                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-                loader: 'file-loader' 
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader'
             },
             {
                 test: /\.(jpg|png)$/,
@@ -72,11 +91,18 @@ module.exports = {
             inject: true,
             template: path.resolve(__dirname, 'src/index.pug')
         }),
-        new HtmlWebpackPlugin({
-            inject: true,
-            template: path.resolve(__dirname, 'src/pages/list.pug'),
-            filename: path.resolve(__dirname, 'dist/pages/list.html')
-        }),
         extractPlugin
     ],
 };
+
+let pugs = getPugsFromFolder('src/pages');
+
+pugs.map(pug => {
+    config.plugins.push(new HtmlWebpackPlugin({
+        inject: true,
+        template: path.resolve(__dirname, `src/pages/${pug}.pug`),
+        filename: path.resolve(__dirname, `dist/pages/${pug}.html`)
+    }))   
+});
+
+module.exports = config
