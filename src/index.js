601b3888e6bf1d2205a5f6fc7cb4b6657568a9df
Dmitry Pintak <poloabusesuckaz@gmail.com>
2017-09-19 18:51:13 +0300

    webpack fixes and events page

--- src/index.js
+++
@@ -1,8 +0,0 @@
-//import stylesheets
-import './sass/index.sass';
-import './resources/team.jpg';
-
-$(document).ready(function(){
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
-    $('.parallax').parallax();
- });
